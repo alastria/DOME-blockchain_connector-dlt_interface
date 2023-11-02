5b09e2268113709c6d643c9f91eb9080cb767b56
alejandro.nieto <alejandro.nieto@madisonmk.com>
2023-11-02 17:33:39 +0100

    Reformatted code. Deleted some debug logs.

--- src/api/DLTInterface.ts
+++ src/api/DLTInterface.ts
@@ -1,8 +1,8 @@
 import { debug } from "debug";
 import { ethers } from "ethers";
 import {
-    domeEventsContractABI as domeEventsContractABI,
-    domeEventsContractAddress as domeEventsContractAddress,
+  domeEventsContractABI as domeEventsContractABI,
+  domeEventsContractAddress as domeEventsContractAddress,
 } from "../utils/const";
 import axios from "axios";
 
@@ -21,39 +21,43 @@ const errorLog = debug("DLT Interface Service:error ");
  * @param req the HTTP request.
  */
 export async function connectToNode(
-    rpcAddress: string,
-    userEthereumAddress: string,
-    req: any
+  rpcAddress: string,
+  userEthereumAddress: string,
+  req: any
 ) {
-    if(rpcAddress === null || rpcAddress === undefined){
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
-    if(userEthereumAddress === null || userEthereumAddress === undefined){
-        throw new IllegalArgumentError("The ethereum address is null.");
-    }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
+  if (userEthereumAddress === null || userEthereumAddress === undefined) {
+    throw new IllegalArgumentError("The ethereum address is null.");
+  }
 
-    debugLog(">>> Connecting to blockchain node...");
-    // Entry parameters in method.
-    debugLog("  > rpcAddress: " + rpcAddress);
-    debugLog("  > userEthereumAddress: " + userEthereumAddress);
+  debugLog(">>> Connecting to blockchain node...");
+  // Entry parameters in method.
+  debugLog("  > rpcAddress: " + rpcAddress);
+  debugLog("  > userEthereumAddress: " + userEthereumAddress);
 
-    let provider;
-    try {
-        provider = new ethers.providers.JsonRpcProvider(rpcAddress);
-    } catch (error) {
-        errorLog(" > !! Error connecting to the blockchain");
-        throw error;
-    }
+  let provider;
+  try {
+    provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+  } catch (error) {
+    errorLog(" > !! Error connecting to the blockchain");
+    throw error;
+  }
 
-    debugLog("  > Provider: " + JSON.stringify(provider));
-    debugLog("  > Provider Network: " + JSON.stringify(await provider.getNetwork()));
-    // Registry req parameters in session.
-    debugLog("  > req.session: " + JSON.stringify(req.session));
-    debugLog("  > req.headers: " + JSON.stringify(req.headers));
-    req.session.provider = provider;
-    req.session.userEthereumAddress = userEthereumAddress;
-    req.session.rpcAddress = rpcAddress;
-    debugLog("  > Stored blockchain node configuration for this user's session (provider and public key).");
+  debugLog("  > Provider: " + JSON.stringify(provider));
+  debugLog(
+    "  > Provider Network: " + JSON.stringify(await provider.getNetwork())
+  );
+  // Registry req parameters in session.
+  debugLog("  > req.session: " + JSON.stringify(req.session));
+  debugLog("  > req.headers: " + JSON.stringify(req.headers));
+  req.session.provider = provider;
+  req.session.userEthereumAddress = userEthereumAddress;
+  req.session.rpcAddress = rpcAddress;
+  debugLog(
+    "  > Stored blockchain node configuration for this user's session (provider and public key)."
+  );
 }
 
 /**
@@ -66,62 +70,63 @@ export async function connectToNode(
  * @param rpcAddress the address of the blockchain node
  */
 export async function publishDOMEEvent(
-    eventType: string,
-    dataLocation: string,
-    relevantMetadata: Array<string>,
-    userEthereumAddress: string,
-    rpcAddress: string,
+  eventType: string,
+  dataLocation: string,
+  relevantMetadata: Array<string>,
+  userEthereumAddress: string,
+  rpcAddress: string
 ) {
-    if(eventType === null || eventType === undefined){
-        throw new IllegalArgumentError("The eventType is null.");
-    }
-    if(dataLocation === null || dataLocation === undefined){
-        throw new IllegalArgumentError("The dataLocation is null.");
-    }
-    if(userEthereumAddress === null || userEthereumAddress === undefined){
-        throw new IllegalArgumentError("The user ethereum address is null.");
-    }
-    if(rpcAddress === null || rpcAddress === undefined){
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
-    
-    try{
-        debugLog(">>> Publishing event to blockchain node...");
+  if (eventType === null || eventType === undefined) {
+    throw new IllegalArgumentError("The eventType is null.");
+  }
+  if (dataLocation === null || dataLocation === undefined) {
+    throw new IllegalArgumentError("The dataLocation is null.");
+  }
+  if (userEthereumAddress === null || userEthereumAddress === undefined) {
+    throw new IllegalArgumentError("The user ethereum address is null.");
+  }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
 
-        debugLog("  > Entry Data:", {
-            userEthereumAddress,
-            eventType,
-            dataLocation,
-            relevantMetadata
-        });
-        const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+  try {
+    debugLog(">>> Publishing event to blockchain node...");
 
-        //TODO: Secure PrivateKey
-        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
-        debugLog("  > Ethereum Address of event publisher: ", wallet.address);
+    debugLog("  > Entry Data:", {
+      userEthereumAddress,
+      eventType,
+      dataLocation,
+      relevantMetadata,
+    });
 
-        const domeEventsContractWithSigner = new ethers.Contract(
-            domeEventsContractAddress,
-            domeEventsContractABI,
-            wallet
-        );
-        debugLog("  > Ethereum Contract: ", domeEventsContractWithSigner.address);
-        debugLog("  > Ethereum Remittent: ", userEthereumAddress);
+    const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
 
-        debugLog("  > Publishing event to blockchain node...");
-        const tx = await domeEventsContractWithSigner.emitNewEvent(
-            userEthereumAddress,
-            eventType,
-            dataLocation,
-            relevantMetadata
-        );
-        debugLog("  > Transaction waiting to be mined...");
-        await tx.wait();
-        debugLog("  > Transaction executed:\n" + JSON.stringify(tx));
-    } catch (error){
-        errorLog(" > !! Error in publishDOMEEvent");
-        throw error;
-    }
+    //TODO: Secure PrivateKey
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+    debugLog("  > Ethereum Address of event publisher: ", wallet.address);
+
+    const domeEventsContractWithSigner = new ethers.Contract(
+      domeEventsContractAddress,
+      domeEventsContractABI,
+      wallet
+    );
+    debugLog("  > Ethereum Contract: ", domeEventsContractWithSigner.address);
+    debugLog("  > Ethereum Remittent: ", userEthereumAddress);
+
+    debugLog("  > Publishing event to blockchain node...");
+    const tx = await domeEventsContractWithSigner.emitNewEvent(
+      userEthereumAddress,
+      eventType,
+      dataLocation,
+      relevantMetadata
+    );
+    debugLog("  > Transaction waiting to be mined...");
+    await tx.wait();
+    debugLog("  > Transaction executed:\n" + JSON.stringify(tx));
+  } catch (error) {
+    errorLog(" > !! Error in publishDOMEEvent");
+    throw error;
+  }
 }
 
 /**
@@ -134,78 +139,109 @@ export async function publishDOMEEvent(
  * @param userEthereumAddress the user's Ethereum address.
  */
 export function subscribeToDOMEEvents(
-    eventTypes: string[],
-    rpcAddress: string,
-    notificationEndpoint: string,
-    userEthereumAddress: string
+  eventTypes: string[],
+  rpcAddress: string,
+  notificationEndpoint: string,
+  userEthereumAddress: string
 ) {
-    if(eventTypes === null || eventTypes === undefined){
-        throw new IllegalArgumentError("The eventType is null.");
-    }
-    if(eventTypes.length === 0){
-        throw new IllegalArgumentError("No eventTypes indicated for subscription.");
-    }
-    if(rpcAddress === null || rpcAddress === undefined){
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
-    if(notificationEndpoint === null || notificationEndpoint === undefined){
-        throw new IllegalArgumentError("The notificationEndpoint is null.");
-    }
+  if (eventTypes === null || eventTypes === undefined) {
+    throw new IllegalArgumentError("The eventType is null.");
+  }
+  if (eventTypes.length === 0) {
+    throw new IllegalArgumentError("No eventTypes indicated for subscription.");
+  }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
+  if (notificationEndpoint === null || notificationEndpoint === undefined) {
+    throw new IllegalArgumentError("The notificationEndpoint is null.");
+  }
 
-    try{
-        debugLog(">>> Subscribing to DOME Events...");
+  try {
+    debugLog(">>> Subscribing to DOME Events...");
 
-        const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
-        const DOMEEventsContract = new ethers.Contract(domeEventsContractAddress, domeEventsContractABI, provider);
-        debugLog(" > Contract with address " + domeEventsContractAddress + " loaded");
-        debugLog(" > User requests to subscribe to events..." + eventTypes.join(", "));
-        debugLog(" > Listening to events...");
+    const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+    const DOMEEventsContract = new ethers.Contract(
+      domeEventsContractAddress,
+      domeEventsContractABI,
+      provider
+    );
+    debugLog(
+      " > Contract with address " + domeEventsContractAddress + " loaded"
+    );
+    debugLog(
+      " > User requests to subscribe to events..." + eventTypes.join(", ")
+    );
+    debugLog(" > Listening to events...");
 
-        DOMEEventsContract.on("EventDOMEv1", (index, timestamp, origin, eventType, dataLocation, metadata) => {
-            if (eventTypes.includes(eventType)) {
-                const eventContent = {
-                    id: index,
-                    publisherAddress: origin,
-                    eventType: eventType,
-                    timestamp: timestamp,
-                    dataLocation: dataLocation,
-                    relevantMetadata: metadata
-                }
+    DOMEEventsContract.on(
+      "EventDOMEv1",
+      (index, timestamp, origin, eventType, dataLocation, metadata) => {
+        if (eventTypes.includes(eventType)) {
+          const eventContent = {
+            id: index,
+            publisherAddress: origin,
+            eventType: eventType,
+            timestamp: timestamp,
+            dataLocation: dataLocation,
+            relevantMetadata: metadata,
+          };
 
-                debugLog(" > Event Content:", {
-                    index,
-                    timestamp,
-                    origin,
-                    eventType,
-                    dataLocation,
-                    metadata
-                });
+          debugLog(" > Event Content:", {
+            index,
+            timestamp,
+            origin,
+            eventType,
+            dataLocation,
+            metadata,
+          });
 
-                debugLog(" > Event emitted: " + eventType + " with args: " + JSON.stringify(eventContent));
-                debugLog(" > Checking EventType " + eventContent.eventType + " with the interest for the user " + eventType);
+          debugLog(
+            " > Event emitted: " +
+              eventType +
+              " with args: " +
+              JSON.stringify(eventContent)
+          );
+          debugLog(
+            " > Checking EventType " +
+              eventContent.eventType +
+              " with the interest for the user " +
+              eventType
+          );
 
-
-                if (eventContent.publisherAddress != userEthereumAddress) {
-                    const headers = {
-                        'Content-Type': 'application/json', // Set the Content-Type header to JSON
-                    };
-                    debugLog(" > Sending notification to endpoint: " + notificationEndpoint);
-                    debugLog(" > Notification Content: " + JSON.stringify(eventContent));
-                    axios.post(notificationEndpoint, JSON.stringify(eventContent), { headers })
-                        .then(response => {
-                            debugLog(" > Response from notification endpoint: " + response.status);
-                        })
-                        .catch(error => {
-                            errorLog(" > !! Error from notification endpoint:\n" + error);
-                            throw new NotificationEndpointError("Can't connect to the notification endpoint.");
-                        });
-                } else {
-                    debugLog(" > This event is not of interest for the user.");
-                }
-            }
-        });
-    } catch (error) {
-        errorLog(" > !! Error subscribing to DOME Events");
-        throw error;
-    }
+          if (eventContent.publisherAddress != userEthereumAddress) {
+            const headers = {
+              "Content-Type": "application/json", // Set the Content-Type header to JSON
+            };
+            debugLog(
+              " > Sending notification to endpoint: " + notificationEndpoint
+            );
+            debugLog(
+              " > Notification Content: " + JSON.stringify(eventContent)
+            );
+            axios
+              .post(notificationEndpoint, JSON.stringify(eventContent), {
+                headers,
+              })
+              .then((response) => {
+                debugLog(
+                  " > Response from notification endpoint: " + response.status
+                );
+              })
+              .catch((error) => {
+                errorLog(" > !! Error from notification endpoint:\n" + error);
+                throw new NotificationEndpointError(
+                  "Can't connect to the notification endpoint."
+                );
+              });
+          } else {
+            debugLog(" > This event is not of interest for the user.");
+          }
+        }
+      }
+    );
+  } catch (error) {
+    errorLog(" > !! Error subscribing to DOME Events");
+    throw error;
+  }
 }
