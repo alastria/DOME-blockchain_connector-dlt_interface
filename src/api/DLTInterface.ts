d9359eac29b37433825091ec67d0aeaf102a6a03
alejandro.nieto <alejandro.nieto@madisonmk.com>
2024-01-04 11:10:14 +0100

    Improved the complexity of code as proposed by sonarcloud. Added more sleep time for subscription hooks to not fail when testing.

--- src/api/DLTInterface.ts
+++ src/api/DLTInterface.ts
@@ -1,8 +1,8 @@
 import { debug } from "debug";
 import { ethers } from "ethers";
 import {
-    domeEventsContractABI, 
-    domeEventsContractAddress,
+  domeEventsContractABI,
+  domeEventsContractAddress,
 } from "../utils/const";
 import axios from "axios";
 
@@ -22,42 +22,42 @@ export async function connectToNode(rpcAddress: string, iss: string, req: any) {
   if (rpcAddress === "") {
     throw new IllegalArgumentError("The rpc address is blank.");
   }
-    if (rpcAddress === null || rpcAddress === undefined) {
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
   if (iss === "") {
     throw new IllegalArgumentError("The iss identifier is blank.");
   }
-    if (iss === null || iss === undefined) {
-        throw new IllegalArgumentError("The iss identifier is null.");
-    }
+  if (iss === null || iss === undefined) {
+    throw new IllegalArgumentError("The iss identifier is null.");
+  }
 
-    debugLog(">>> Connecting to blockchain node...");
-    // Entry parameters in method.
-    debugLog("  > rpcAddress: " + rpcAddress);
-    debugLog("  > iss: " + iss);
+  debugLog(">>> Connecting to blockchain node...");
+  // Entry parameters in method.
+  debugLog("  > rpcAddress: " + rpcAddress);
+  debugLog("  > iss: " + iss);
 
-    let provider;
-    try {
-        provider = new ethers.providers.JsonRpcProvider(rpcAddress);
-    } catch (error) {
-        errorLog(" > !! Error connecting to the blockchain");
-        throw error;
-    }
+  let provider;
+  try {
+    provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+  } catch (error) {
+    errorLog(" > !! Error connecting to the blockchain");
+    throw error;
+  }
 
-    debugLog("  > Provider: " + JSON.stringify(provider));
-    debugLog(
-        "  > Provider Network: " + JSON.stringify(await provider.getNetwork())
-    );
-    // Registry req parameters in session.
-    debugLog("  > req.session: " + JSON.stringify(req.session));
-    debugLog("  > req.headers: " + JSON.stringify(req.headers));
-    req.session.provider = provider;
-    req.session.iss = iss;
-    req.session.rpcAddress = rpcAddress;
-    debugLog(
-        "  > Stored blockchain node configuration for this user's session (provider and public key)."
-    );
+  debugLog("  > Provider: " + JSON.stringify(provider));
+  debugLog(
+    "  > Provider Network: " + JSON.stringify(await provider.getNetwork())
+  );
+  // Registry req parameters in session.
+  debugLog("  > req.session: " + JSON.stringify(req.session));
+  debugLog("  > req.headers: " + JSON.stringify(req.headers));
+  req.session.provider = provider;
+  req.session.iss = iss;
+  req.session.rpcAddress = rpcAddress;
+  debugLog(
+    "  > Stored blockchain node configuration for this user's session (provider and public key)."
+  );
 }
 
 /**
@@ -71,77 +71,77 @@ export async function connectToNode(rpcAddress: string, iss: string, req: any) {
  * @param entityIDHash entity identifier hash
  */
 export async function publishDOMEEvent(
-    eventType: string,
-    dataLocation: string,
-    relevantMetadata: Array<string>,
-    iss: string,
-    entityIDHash: string,
-    previousEntityHash: string,
-    rpcAddress: string
+  eventType: string,
+  dataLocation: string,
+  relevantMetadata: Array<string>,
+  iss: string,
+  entityIDHash: string,
+  previousEntityHash: string,
+  rpcAddress: string
 ) {
   if (eventType === "") {
     throw new IllegalArgumentError("The eventType is blank.");
   }
-    if (eventType === null || eventType === undefined) {
-        throw new IllegalArgumentError("The eventType is null.");
-    }
-    if (dataLocation === null || dataLocation === undefined) {
-        throw new IllegalArgumentError("The dataLocation is null.");
-    }
-    if (iss === null || iss === undefined) {
-        throw new IllegalArgumentError("The iss identifier is null.");
-    }
-    if (entityIDHash === null || entityIDHash === undefined) {
-        throw new IllegalArgumentError("The entity identifier hash is null.");
-    }
-    if (previousEntityHash === null || previousEntityHash === undefined) {
-        throw new IllegalArgumentError("The previousEntityHash is null.");
-    }
-    if (rpcAddress === null || rpcAddress === undefined) {
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
+  if (eventType === null || eventType === undefined) {
+    throw new IllegalArgumentError("The eventType is null.");
+  }
+  if (dataLocation === null || dataLocation === undefined) {
+    throw new IllegalArgumentError("The dataLocation is null.");
+  }
+  if (iss === null || iss === undefined) {
+    throw new IllegalArgumentError("The iss identifier is null.");
+  }
+  if (entityIDHash === null || entityIDHash === undefined) {
+    throw new IllegalArgumentError("The entity identifier hash is null.");
+  }
+  if (previousEntityHash === null || previousEntityHash === undefined) {
+    throw new IllegalArgumentError("The previousEntityHash is null.");
+  }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
 
-    try {
-        debugLog(">>> Publishing event to blockchain node...");
+  try {
+    debugLog(">>> Publishing event to blockchain node...");
 
-        debugLog("  > Entry Data:", {
-            iss,
-            entityIDHash,
-            previousEntityHash,
-            eventType,
-            dataLocation,
-            relevantMetadata,
-        });
+    debugLog("  > Entry Data:", {
+      iss,
+      entityIDHash,
+      previousEntityHash,
+      eventType,
+      dataLocation,
+      relevantMetadata,
+    });
 
-        const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+    const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
 
-        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
-        debugLog("  > Ethereum Address of event publisher: ", wallet.address);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+    debugLog("  > Ethereum Address of event publisher: ", wallet.address);
 
-        const domeEventsContractWithSigner = new ethers.Contract(
-            domeEventsContractAddress,
-            domeEventsContractABI,
-            wallet
-        );
+    const domeEventsContractWithSigner = new ethers.Contract(
+      domeEventsContractAddress,
+      domeEventsContractABI,
+      wallet
+    );
 
-        debugLog("  > Ethereum Contract: ", domeEventsContractWithSigner.address);
-        debugLog("  > Ethereum Remittent: ", iss);
+    debugLog("  > Ethereum Contract: ", domeEventsContractWithSigner.address);
+    debugLog("  > Ethereum Remittent: ", iss);
 
-        debugLog("  > Publishing event to blockchain node...");
-        const tx = await domeEventsContractWithSigner.emitNewEvent(
-            iss,
-            entityIDHash,
-            eventType,
-            dataLocation,
-            relevantMetadata
-        );
-        debugLog("  > Transaction waiting to be mined...");
-        await tx.wait();
-        debugLog("  > Transaction executed:\n" + JSON.stringify(tx));
-    } catch (error) {
-        errorLog(" > !! Error in publishDOMEEvent");
-        throw error;
-    }
+    debugLog("  > Publishing event to blockchain node...");
+    const tx = await domeEventsContractWithSigner.emitNewEvent(
+      iss,
+      entityIDHash,
+      eventType,
+      dataLocation,
+      relevantMetadata
+    );
+    debugLog("  > Transaction waiting to be mined...");
+    await tx.wait();
+    debugLog("  > Transaction executed:\n" + JSON.stringify(tx));
+  } catch (error) {
+    errorLog(" > !! Error in publishDOMEEvent");
+    throw error;
+  }
 }
 
 /**
@@ -155,50 +155,52 @@ export async function publishDOMEEvent(
  * @param ownIss the organization identifier hash
  */
 export function subscribeToDOMEEvents(
-    eventTypes: string[],
-    rpcAddress: string,
-    ownIss: string,
-    notificationEndpoint?: string,
-    handler?: (event: object) => void
+  eventTypes: string[],
+  rpcAddress: string,
+  ownIss: string,
+  notificationEndpoint?: string,
+  handler?: (event: object) => void
 ) {
-    if (eventTypes === null || eventTypes === undefined) {
-        throw new IllegalArgumentError("The eventType is null.");
-    }
-    if (eventTypes.length === 0) {
-        throw new IllegalArgumentError("No eventTypes indicated for subscription.");
-    }
-    if (eventTypes.includes("")) {
-        throw new IllegalArgumentError("Blank eventTypes indicated for subscription.");
-    }
-    if (rpcAddress === null || rpcAddress === undefined) {
-        throw new IllegalArgumentError("The rpc address is null.");
-    }
-    if (ownIss === "") {
-        throw new IllegalArgumentError("The ownIss is blank.");
-    }
-    if (ownIss === null || ownIss === undefined) {
-        throw new IllegalArgumentError("The ownIss is null.");
-    }
+  if (eventTypes === null || eventTypes === undefined) {
+    throw new IllegalArgumentError("The eventType is null.");
+  }
+  if (eventTypes.length === 0) {
+    throw new IllegalArgumentError("No eventTypes indicated for subscription.");
+  }
+  if (eventTypes.includes("")) {
+    throw new IllegalArgumentError(
+      "Blank eventTypes indicated for subscription."
+    );
+  }
+  if (rpcAddress === null || rpcAddress === undefined) {
+    throw new IllegalArgumentError("The rpc address is null.");
+  }
+  if (ownIss === "") {
+    throw new IllegalArgumentError("The ownIss is blank.");
+  }
+  if (ownIss === null || ownIss === undefined) {
+    throw new IllegalArgumentError("The ownIss is null.");
+  }
 
-    try {
-        debugLog(">>> Subscribing to DOME Events...");
+  try {
+    debugLog(">>> Subscribing to DOME Events...");
 
-        const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
-        const DOMEEventsContract = new ethers.Contract(
-            domeEventsContractAddress,
-            domeEventsContractABI,
-            provider
-        );
-        debugLog(
-            " > Contract with address " + domeEventsContractAddress + " loaded"
-        );
-        debugLog(
-            " > User requests to subscribe to events..." + eventTypes.join(", ")
-        );
-        debugLog(" > Listening to events...");
+    const provider = new ethers.providers.JsonRpcProvider(rpcAddress);
+    const DOMEEventsContract = new ethers.Contract(
+      domeEventsContractAddress,
+      domeEventsContractABI,
+      provider
+    );
+    debugLog(
+      " > Contract with address " + domeEventsContractAddress + " loaded"
+    );
+    debugLog(
+      " > User requests to subscribe to events..." + eventTypes.join(", ")
+    );
+    debugLog(" > Listening to events...");
 
-        DOMEEventsContract.on(
-            "EventDOMEv1",
+    DOMEEventsContract.on(
+      "EventDOMEv1",
       (
         index,
         timestamp,
@@ -208,42 +210,52 @@ export function subscribeToDOMEEvents(
         dataLocation,
         metadata
       ) => {
-        if (!eventTypes.includes(eventType)) {
+        const eventContent = {
+          id: index,
+          publisherAddress: origin,
+          entityIDHash: entityIDHash,
+          previousEntityIDHash: entityIDHash,
+          eventType: eventType,
+          timestamp: timestamp,
+          dataLocation: dataLocation,
+          relevantMetadata: metadata,
+        };
+
+        abstractDOMEEventsHandler(eventContent, eventTypes, ownIss, notificationEndpoint, handler);
+      }
+    );
+  } catch (error) {
+    errorLog(" > !! Error subscribing to DOME Events");
+    throw error;
+  }
+}
+
+/**
+ * Abstract event handler for DOME events that performs all the common handling and calls more specific handlers.
+ * @param eventContent the DOME event to be handled.
+ * @param eventTypes the event types of interest.
+ * @param ownIss the iss identifier of the entity performing the subscription.
+ * @param notificationEndpoint the endpoint to be notified of the event.
+ * @param handler the specific handler for the event.
+ */
+function abstractDOMEEventsHandler(eventContent: any, eventTypes: string[], ownIss: string, notificationEndpoint?: string, handler?: (event: object) => void){
+        if (!eventTypes.includes(eventContent.eventType)) {
           return;
         }
-                    const eventContent = {
-                        id: index,
-                        publisherAddress: origin,
-                        entityIDHash: entityIDHash,
-                        previousEntityIDHash: entityIDHash,
-                        eventType: eventType,
-                        timestamp: timestamp,
-                        dataLocation: dataLocation,
-                        relevantMetadata: metadata,
-                    };
 
-                    debugLog(" > Event Content:", {
-                        index,
-                        timestamp,
-                        origin,
-                        entityIDHash,
-                        eventType,
-                        dataLocation,
-                        metadata,
-                    });
-
-                    debugLog(
-                        " > Event emitted: " +
-                        eventType +
-                        " with args: " +
-                        JSON.stringify(eventContent)
-                    );
-                    debugLog(
-                        " > Checking EventType " +
-                        eventContent.eventType +
-                        " with the interest for the user " +
-                        eventType
-                    );
+        debugLog(" > Event Content:", eventContent);
+        debugLog(
+          " > Event emitted: " +
+            eventContent.eventType +
+            " with args: " +
+            JSON.stringify(eventContent)
+        );
+        debugLog(
+          " > Checking EventType " +
+            eventContent.eventType +
+            " with the interest for the user " +
+            eventContent.eventType
+        );
 
         if (eventContent.publisherAddress === ownIss) {
           debugLog(" > This event is not of interest for the user.");
@@ -255,12 +267,6 @@ export function subscribeToDOMEEvents(
         if (handler != undefined) {
           handler(eventContent);
         }
-      }
-    );
-  } catch (error) {
-    errorLog(" > !! Error subscribing to DOME Events");
-    throw error;
-  }
 }
 
 /**
@@ -272,22 +278,22 @@ function notifyEndpointDOMEEventsHandler(
   event: object,
   notificationEndpoint: string
 ) {
-                        const headers = {
-                            "Content-Type": "application/json", // Set the Content-Type header to JSON
-                        };
+  const headers = {
+    "Content-Type": "application/json", // Set the Content-Type header to JSON
+  };
   debugLog(" > Sending notification to endpoint: " + notificationEndpoint);
   debugLog(" > Notification Content: " + JSON.stringify(event));
-                        axios
+  axios
     .post(notificationEndpoint, JSON.stringify(event), {
-                                headers,
-                            })
-                            .then((response) => {
+      headers,
+    })
+    .then((response) => {
       debugLog(" > Response from notification endpoint: " + response.status);
-                            })
-                            .catch((error) => {
-                                errorLog(" > !! Error from notification endpoint:\n" + error);
-                                throw new NotificationEndpointError(
-                                    "Can't connect to the notification endpoint."
-                                );
-                            });
+    })
+    .catch((error) => {
+      errorLog(" > !! Error from notification endpoint:\n" + error);
+      throw new NotificationEndpointError(
+        "Can't connect to the notification endpoint."
+      );
+    });
 }
