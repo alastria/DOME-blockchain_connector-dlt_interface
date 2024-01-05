1d7a58221c5c33c43f385379e7e7d6612bd9de70
alejandro.nieto <alejandro.nieto@madisonmk.com>
2024-01-05 08:19:44 +0100

    Added deletion of index field for the returned events.

--- src/api/DLTInterface.ts
+++ src/api/DLTInterface.ts
@@ -388,19 +388,26 @@ export async function getActiveDOMEEventsByDate(
     }
   }
 
+  debugLog("The active DOME Events to be returned are the following:\n");
   let allActiveDOMEEvents: object[] = [];
   allActiveEvents.forEach((event) => {
     let eventJson = JSON.parse(JSON.stringify(event));
-    console.log(JSON.stringify(event));
     let eventIndexHex = event.args![0]._hex;
     let eventTimestampHex = event.args![1]._hex;
     eventJson.args![0] = BigNumber.from(eventIndexHex).toNumber();
     eventJson.args![1] = BigNumber.from(eventTimestampHex).toNumber() * 1000;
+    debugLog(eventJson.args);
+    // delete active field
     delete eventJson.args[7];
+    // delete index field
+    delete eventJson.args[0];
+    eventJson.args.forEach((arg: any) => {
+
+        
+    });
+
     allActiveDOMEEvents.push(eventJson.args);
   });
 
-  //debugLog("The active DOME Events to be returned are the following:\n");
-  //debugLog(await allActiveEvents);
   return allActiveDOMEEvents;
 }
